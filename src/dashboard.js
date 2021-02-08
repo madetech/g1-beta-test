2506a5b6de739fce2c9af36d06a9e6a671e18da8
Emma Blott <blott23@gmail.com>
2021-02-08 18:29:53 +0000

    more auth

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
-function Dashboard() {
-  return (
-    <div className="App">
+const Dashboard = () => (
+    <div>
       <header className="App-header">
           This is the Dashboard
       </header>
     </div>
   );
-}
 
-export default App;
+
+export default withAuthenticator(Dashboard);
