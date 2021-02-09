992f59bcdc0d954f5a5c5337b620133117dde622
Emma Blott <blott23@gmail.com>
2021-02-09 11:21:23 +0000

    auth changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import Amplify, { Auth, Hub } from 'aws-amplify';
+import awsconfig from './aws-exports';
 import {
   BrowserRouter as Router,
   Switch,
@@ -6,58 +8,84 @@ import {
   Link
 } from "react-router-dom";
 
-// import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import Dashboard from './dashboard.js'
 
-// const App = () => (
-//   <div>
-//     <AmplifySignOut />
-//     My App
-//   </div>
-// );
-
-import Amplify, { Auth } from 'aws-amplify';
-import awsconfig from './aws-exports';
 Amplify.configure(awsconfig);
 
+function App() {
+  const [user, setUser] = useState(null);
 
-export default function App() {
+  useEffect(() => {
+    Hub.listen('auth', ({ payload: { event, data } }) => {
+      switch (event) {
+        case 'signIn':
+        case 'cognitoHostedUI':
+          getUser().then(userData => setUser(userData));
+          break;
+        case 'signOut':
+          setUser(null);
+          break;
+        case 'signIn_failure':
+        case 'cognitoHostedUI_failure':
+          console.log('Sign in failure', data);
+          break;
+      }
+    });
+
+    getUser().then(userData => setUser(userData));
+  }, []);
+
+  function getUser() {
+    return Auth.currentAuthenticatedUser()
+      .then(userData => userData)
+      .catch(() => console.log('Not signed in'));
+  }
+
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/dashboard">Dashboard</Link>
-            </li>
-          </ul>
-        </nav>
+    <div>
+      <p>User: {user ? JSON.stringify(user.attributes) : 'None'}</p>
+      {user ? (
+        <button onClick={() => Auth.signOut()}>Sign Out</button>
+      ) : (
+        <button onClick={() => Auth.federatedSignIn()}>Sign In</button>
+      )}
+    
+  <Router>
+    <div>
+      
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/dashboard">
+          <Dashboard user={user} />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+  </div>
+);
 }
 
 function Home() {
-  return <h2>Home</h2>;
+  return (
+  <div>
+  <h2>Home</h2>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+      </ul>
+    </nav>
+  </div>
+  );
 }
 
-function Dashboard() 
-
-
-
-{
-  return <h2>This is the Dashboard</h2>;
-}
+export default App;
--- src/dashboard.js
+++ src/dashboard.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
-const Dashboard = () => (
+function Dashboard({user}) {
+  if (!user) return (<div>Please log in to see the dashboard</div> )  
+  return(
+
     <div>
-      <header className="App-header">
-          This is the Dashboard
-      </header>
-      <AmplifySignOut/>
+      <h1>
+          This is the Dashboard<br/>
+          You are {user}
+      </h1>
     </div>
-  );
+)
+};
 
 
-export default withAuthenticator(Dashboard);
+export default Dashboard;
